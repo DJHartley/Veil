d3293490291bb8aa505dea2404c529a37eebb8a9
moloch <moloch>
2013-06-30 19:51:56 -0700

    Updated readme and setup scripts

---
+++ setup/web_setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#    Copyright 2012 Root the Box
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+if [ "$(id -u)" != "0" ]; then
+	echo "[!] This script must be run as root." 1>&2
+	exit 1
+fi
+
+echo "[*] Installing pip/gcc/etc ..."
+apt-get install python python-dev python-pip build-essential
+# Install MinGW for C payloads
+apt-get install mingw-w64
+
+# install mono for C# payloads
+apt-get install monodoc-browser
+apt-get install monodevelop
+apt-get install mono-mcs
+
+
+echo "[*] Installing packages ..."
+apt-get install mysql-server memcached libmemcached-dev python-mysqldb python-mysqldb-dbg python-pycurl python-recaptcha zlib1g-dev
+
+echo "[*] Installing python libs ..."
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+sh "$DIR/python_python.sh"
+
+echo ""
+echo "[*] Setup Completed."
